ce433fd92f2fae62fd32a07b72365adf2df28022
badar <badarshahzad54@gmail.com>
2016-10-14 02:40:27 +0500

    v0.1: First interface gui added

--- SEGP Group10/src/play/area/PlayArea.java
+++ SEGP Group10/src/play/area/PlayArea.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package segp.group10;
+package play.area;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author badar
  */
-public class SEGPGroup10 extends Application {
+public class PlayArea extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
