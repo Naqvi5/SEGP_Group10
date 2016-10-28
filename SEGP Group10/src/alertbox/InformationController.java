18205d789d369f70670d68f2b76adf475dd7dae2
badar <badarshahzad54@gmail.com>
2016-10-28 23:42:19 +0500

    alertbox package added for popups

---
+++ SEGP Group10/src/alertbox/InformationController.java
@@ -0,0 +1,15 @@
+package alertbox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class InformationController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
